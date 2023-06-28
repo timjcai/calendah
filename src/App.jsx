dba3b8e23ac9aa12d5164b877ffd49172a977397
Tim Cai <timcai@Mac-mini.gateway>
2023-06-28 17:44:22 +1000

    Feat: created react app and implemented Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen bg-red-300">
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+    </div>
+
+  )
+}
