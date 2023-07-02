7be05b77ed29fceed03a251d3d99cb3390c2e738
timjcai <tim.j.cai@gmail.com>
2023-07-02 21:12:24 +1000

    Feat: Implemented React Router (basic)
    What?
    (1) created Browser router for Settings, View and New pages
    
    Next Up:
    (1) create a navbar to navigate between the three
    (2) create layout for new event page -> from

---
+++ src/pages/New.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const New = () => {
+  return (
+    <div>
+      <h1>New Event Page</h1>
+    </div>
+  )
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Settings = () => {
+  return (
+    <div>
+      <h1>Settings page</h1>
+    </div>
+  )
+}
+
