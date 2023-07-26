b9efb28f92ff9fdd79881c0b6114aa9fa7271ce3
timjcai <tim.j.cai@gmail.com>
2023-07-26 19:27:50 +1000

    WIP: SelectPicker Styles
    (1) removed default styling of input
    (2) added basic styling for popup modal
    
    Up next:
    > move popup selector outside of the input div
    > change state based on mouse interaction with the selector
    > add in RHF implementation

--- src/components/SelectPicker/SelectPicker.tsx
+++ src/components/SelectPicker/SelectPicker.tsx
@@ -16,20 +16,58 @@ export const SelectPicker = ({ label, placeholder, list }) => {
 
     return (
         <div>
-            <SelectInputLabel>{label}</SelectInputLabel>
-            <div>
-                <input value={selectItem} onClick={handlePopup} />
+            <SelectorLabel>{label}</SelectorLabel>
+            <SelectWrapper>
+                <SelectInput value={selectItem} onClick={handlePopup} />
                 <PopupSelector
                     list={list}
                     isHidden={isHidden}
                     handleSelect={handleSelect}
                 ></PopupSelector>
-            </div>
+            </SelectWrapper>
         </div>
     );
 };
 
-export const SelectInputLabel = styled.label``;
+export const SelectInput = styled.input`
+    padding: 0;
+    background: none;
+    border: none;
+    border-radius: 0;
+    outline: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+`;
+
+export const SelectWrapper = styled.div`
+    padding: 16.5px 14px;
+    border: 1px solid grey;
+    border-radius: 4px;
+`;
+
+export const SelectorLabel = styled.label`
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.4375em;
+    letter-spacing: 0.00938em;
+    padding: 0px;
+    display: block;
+    transform-origin: left top;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: calc(133% - 32px);
+    position: absolute;
+    transform: translate(14px, -9px) scale(0.75);
+    transition:
+        color 200ms cubic-bezier(0, 0, 0.2, 1) 0ms,
+        transform 200ms cubic-bezier(0, 0, 0.2, 1) 0ms,
+        max-width 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
+    z-index: 1;
+    pointer-events: auto;
+    user-select: none;
+`;
 
 export const PopupSelector = ({ list, isHidden, handleSelect }) => {
     return (
@@ -84,6 +122,10 @@ export const PopupMenu = styled.ul`
         rgba(0, 0, 0, 0.14) 0px 8px 10px 1px,
         rgba(0, 0, 0, 0.12) 0px 3px 14px 2px;
     border-radius: 4px;
+    background-color: #2f2f2f;
+    color: white;
+    padding-left: 16px;
+    padding-right: 16px;
 `;
 
 export const PopupMenuItem = styled.li`
